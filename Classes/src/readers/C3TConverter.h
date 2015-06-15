a92662f2656c1a1d029a1558e71b28e3a703e589
Tim <Tim>
2015-06-15 19:10:10 +0200

    Materials and Parts added

--- Classes/src/readers/C3TConverter.h
+++ Classes/src/readers/C3TConverter.h
@@ -72,19 +72,72 @@ class C3TConverter : public Reader
 					conv_mesh->parts.push_back(conv_meshPart);
 				}
 
-				//conv_mesh->attributes.value = 0x11;
+				for(cocos2d::MeshVertexAttrib const& coco_meshAtt : coco_meshIt->attribs) {
+					char const* AttributeNames[] = {"UNKNOWN", "VERTEX_ATTRIB_POSITION", "VERTEX_ATTRIB_NORMAL", "VERTEX_ATTRIB_COLOR", "COLORPACKED", "VERTEX_ATTRIB_TANGENT", "VERTEX_ATTRIB_BINORMAL", "VERTEX_ATTRIB_TEX_COORD", "VERTEX_ATTRIB_TEX_COORD1", "VERTEX_ATTRIB_TEX_COORD2", "VERTEX_ATTRIB_TEX_COORD3", "VERTEX_ATTRIB_TEX_COORD4", "VERTEX_ATTRIB_TEX_COORD5", "VERTEX_ATTRIB_TEX_COORD6", "VERTEX_ATTRIB_TEX_COORD7", "VERTEX_ATTRIB_BLEND_WEIGHT", "VERTEX_ATTRIB_BLEND_INDEX", "BLENDWEIGHT2", "BLENDWEIGHT3", "BLENDWEIGHT4", "BLENDWEIGHT5", "BLENDWEIGHT6", "BLENDWEIGHT7"};
+					MeshVertexAttrib conv_att;
+					CCLOG("%i", coco_meshAtt.vertexAttrib);
+					if(coco_meshAtt.vertexAttrib < sizeof(AttributeNames) / sizeof(char const*)) {
+						conv_att.name = AttributeNames[coco_meshAtt.vertexAttrib];
+					} else {
+						conv_att.name = "UNKNOWN";
+					}
+					conv_att.size = coco_meshAtt.size;
+					conv_att.type = "GL_FLOAT";
+					conv_mesh->attributes.attributemap[conv_att.name] = conv_att;
+					//coco_meshAtt.vertexAttrib
+					conv_mesh->attributes.set(coco_meshAtt.vertexAttrib, true);
+				}
 
-				/*for(cocos2d::MeshVertexAttrib const& coco_meshAtt : coco_Mesh->attributes) {
-
-				}*/
+				CCLOG("%i", conv_mesh->attributes.size());
 
 				model->meshes.push_back(conv_mesh);
+			}
+
+			for(cocos2d::NodeData const* coco_node : nodeDatas.nodes) {
+				model->nodes.push_back(loadNode(*coco_node, false, model));
+			}
+			for(cocos2d::NodeData const* coco_node : nodeDatas.skeleton) {
+				model->nodes.push_back(loadNode(*coco_node, true, model));
 			}
 			return true;
 		}
 
 	protected:
 	private:
+		Node* loadNode(cocos2d::NodeData const& pNode, bool skeleton, Model *const &model) {
+			Node *conv_node = new Node;
+			conv_node->id = pNode.id;
+			memcpy(conv_node->transforms, pNode.transform.m, sizeof(conv_node->transforms));
+			conv_node->_skeleton = skeleton;
+			for(cocos2d::NodeData const* coco_node : pNode.children) {
+				conv_node->children.push_back(loadNode(*coco_node, skeleton, model));
+			}
+			for(cocos2d::ModelData const* coco_model : pNode.modelNodeDatas) {
+				NodePart *conv_nodePart = new NodePart;
+				std::vector<Material*>::iterator material = std::find_if(model->materials.begin(),
+				                                                         model->materials.end(),
+				                                                         [&coco_model](Material* const& mat){return mat->id == coco_model->matrialId;});
+				if(material != model->materials.end()) {
+					conv_nodePart->material = *material;
+				} else {
+					conv_nodePart->material = nullptr;
+				}
+
+				conv_nodePart->meshPart = nullptr;
+				for(Mesh const* conv_mesh : model->meshes) {
+					std::vector<MeshPart*>::const_iterator meshPart = std::find_if(conv_mesh->parts.begin(),
+					                                                               conv_mesh->parts.end(),
+					                                                               [&coco_model](MeshPart* const& meshPart){return meshPart->id == coco_model->subMeshId;});
+					if(meshPart != conv_mesh->parts.end()) {
+						conv_nodePart->meshPart = *meshPart;
+						break;
+					}
+				}
+				conv_node->parts.push_back(conv_nodePart);
+			}
+			return conv_node;
+		}
+
 		cocos2d::MeshDatas meshDatas;
 		cocos2d::MaterialDatas materialDatas;
 		cocos2d::NodeDatas nodeDatas;
